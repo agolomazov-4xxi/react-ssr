6a0ef720e055b4e82d97fa3cf523e92144641e9b
agolomazov <agolomazov@4xxi.com>
2018-03-29 14:18:06 +0300

    Add redux for client and server

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,11 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+
 import Routes from './Routes';
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
