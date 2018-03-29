3d5421b37201031443908f190731da36c4c87d1a
agolomazov <agolomazov@4xxi.com>
2018-03-29 19:01:47 +0300

    Add proxy library

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
+import proxy from 'express-http-proxy';
 import { matchRoutes } from 'react-router-config';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
@@ -7,6 +8,12 @@ import Routes from './client/Routes';
 
 const app = express();
 
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecoration(opts) {
+    opts.header['x-forwarder-host'] = 'localhost:3000';
+    return opts;
+  }
+}));
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
