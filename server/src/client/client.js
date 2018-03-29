f002ad3f834f45bf628d71c37c8d40e02b361dc9
agolomazov <agolomazov@4xxi.com>
2018-03-29 12:44:50 +0300

    Add routing on server and client

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,9 +1,12 @@
 // Точка входа в клиентское приложение
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
 ReactDOM.hydrate(
-  <Home/>,
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
   document.getElementById('root')
 );
